27a68c25f9d62107f0e4d4f3225067affda4f313
kvin1087 <kaustubh87@gmail.com>
2017-07-13 22:20:22 -0500

    Added movie list

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -9,12 +9,34 @@
  */
 angular.module('movieManiaApp',['ngRoute'])
 .controller('MainCtrl', function ($scope) {
-    var movie = {
+    /*var movie = {
       title: 'The Revenant',
       image: '/images/revenant.jpg',
       description: 'This movie is based on a lone survivor'
     };
+    */
 
-    $scope.movieTitle = movie.title;
-    $scope.movieDescription = movie.description;
+    var movieList = [
+      {
+        'title' : 'Ace Drummond',
+        'image': './images/ace.jpeg',
+        'description': 'This is based on the comic strip character'
+
+      },
+      {
+        'title' : 'And then there were none',
+        'image': './images/none.jpeg',
+        'description': 'Based on a novel and subsequemt stage play'
+
+      },
+      {
+        'title': 'Captain Kidd',
+        'image' : './images/kidd.png',
+        'description': 'The unhistorical adventures of pirate Captain Kidd'
+      }
+    ];
+
+    $scope.movies = movieList;
+  }).controller('FormCtrl',function($scope){
+      console.log('We have a form controller');
   });
