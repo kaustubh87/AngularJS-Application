aaad7149c32a189ad03b47916d07a1c519384252
kvin1087 <kaustubh87@gmail.com>
2017-07-17 21:13:10 -0500

    Generated a new route using yeoman

---
+++ scripts/controllers/movie.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name movieManiaAppApp.controller:MovieCtrl
+ * @description
+ * # MovieCtrl
+ * Controller of the movieManiaAppApp
+ */
+angular.module('movieManiaAppApp')
+  .controller('MovieCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
