4581f53402af7716d6d3f664416a271e947726a6
kvin1087 <kaustubh87@gmail.com>
2017-07-17 22:03:40 -0500

    added movie list array

--- scripts/controllers/movie.js
+++ scripts/controllers/movie.js
@@ -7,11 +7,38 @@
  * # MovieCtrl
  * Controller of the movieManiaAppApp
  */
-angular.module('movieManiaAppApp')
-  .controller('MovieCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+angular.module('movieManiaApp')
+  .controller('MovieCtrl', function ($scope, $routeParams) {
+    console.debug($routeParams.id);
+    console.debug($routeParams.slug);
+    var id = parseInt($routeParams.id , 10);
+    var movieList = [
+      {
+        id: 23,
+        'title' : 'Ace Drummond',
+        'image': './images/ace.jpeg',
+        'description': 'This is based on the comic strip character'
+
+      },
+      {
+        id: 5,
+        'title' : 'And then there were none',
+        'image': './images/none.jpeg',
+        'description': 'Based on a novel and subsequemt stage play'
+
+      },
+      {
+        id: 43,
+        'title': 'Captain Kidd',
+        'image' : './images/kidd.png',
+        'description': 'The unhistorical adventures of pirate Captain Kidd'
+      }];
+
+        for(var index in movieList){
+          var movie = movieList[index];
+          if(movie.id === id){
+            $scope.movie = movie;
+            break;
+          }
+        }
   });
