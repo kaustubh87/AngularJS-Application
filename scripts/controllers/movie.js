e1de5c37044dc992b37462bd61c43cac5833893a
kvin1087 <kaustubh87@gmail.com>
2017-07-18 15:56:50 -0500

    Debugging values

--- scripts/controllers/movie.js
+++ scripts/controllers/movie.js
@@ -10,7 +10,7 @@
 angular.module('movieManiaApp')
   .controller('MovieCtrl', function ($scope, $routeParams) {
     console.debug($routeParams.id);
-    console.debug($routeParams.slug);
+
     var id = parseInt($routeParams.id , 10);
     var movieList = [
       {
